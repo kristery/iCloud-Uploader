745d7e67aa748fe7af92b672686873f1306ab1eb
Kris Wu <kriswu8021@gmail.com>
2023-05-16 18:38:48 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# iCloud-Uploader
-This script is an automated uploader to iCloud.
+# iCloud Uploader
+
+This script is an automated uploader to iCloud. It monitors a specified directory for any new files and automatically uploads them to the designated iCloud Drive folder. The script categorizes files into images, videos, and others based on their extensions and uploads them to the corresponding subfolders in iCloud Drive.
+
+The script also handles two-factor authentication and two-step verification for iCloud. It prompts the user for verification codes when needed.
+
+## Prerequisites
+
+- Python 3.6 or newer
+- pyicloud library
+- click library
+
+The script depends on the pyicloud library to interact with iCloud and the click library for user-friendly command-line interfaces. You can install these libraries using pip:
+```
+pip install pyicloud click
+```
+## How to Execute
+
+1. Prepare an info file. The script reads username, password, and iCloud Drive folder name from a file. Each piece of information should be on its own line in the file. By default, the script reads from a file named info.txt in the same directory, but you can specify a different file using the -f or --file command-line option.
+
+Example info.txt:
+
+```
+username@example.com
+password123
+my_folder
+```
+
+2. Run the script. If you are using the default info file, you can simply run:
+```
+cd src
+python icloud_uploader.py
+```
+
+If you want to specify a different info file, use the -f or --file option:
+
+```
+cd src
+python icloud_uploader.py -f my_info.txt
+```
+
+## Notes
+
+- The script monitors the current working directory. Any new files in this directory, excluding Python files, are uploaded to iCloud.
+- If a new directory appears in the monitored directory, the script compresses it into a zip file and uploads it.
+- The script creates the iCloud Drive folder if it does not exist. It also creates images, videos, and files subfolders in the iCloud Drive folder.
+- The script checks the monitored directory every minute for new files.
---
+++ src/icloud_uploader.py
@@ -0,0 +1,186 @@
+import argparse
+import os
+import sys
+import time
+import zipfile
+
+from pyicloud import PyiCloudService
+
+
+def read_info(file_path):
+    # Read the file and extract the information
+    with open(file_path, "r") as file:
+        lines = file.readlines()
+        username = lines[0].strip()
+        password = lines[1].strip()
+        folder = lines[2].strip()
+
+    return username, password, folder
+
+
+def login(username, password):
+    # Now you can use the username and password to login
+    api = PyiCloudService(username, password)
+
+    if api.requires_2fa:
+        print("Two-factor authentication required.")
+        code = input("Enter the code you received of one of your approved devices: ")
+        result = api.validate_2fa_code(code)
+        print("Code validation result: %s" % result)
+
+        if not result:
+            print("Failed to verify security code")
+            sys.exit(1)
+
+        if not api.is_trusted_session:
+            print("Session is not trusted. Requesting trust...")
+            result = api.trust_session()
+            print("Session trust result %s" % result)
+
+            if not result:
+                print(
+                    "Failed to request trust. You will likely be prompted for the code again in the coming weeks"
+                )
+    elif api.requires_2sa:
+        import click
+
+        print("Two-step authentication required. Your trusted devices are:")
+
+        devices = api.trusted_devices
+        for i, device in enumerate(devices):
+            print(
+                "  %s: %s"
+                % (i, device.get("deviceName", "SMS to %s" % device.get("phoneNumber")))
+            )
+
+        device = click.prompt("Which device would you like to use?", default=0)
+        device = devices[device]
+        if not api.send_verification_code(device):
+            print("Failed to send verification code")
+            sys.exit(1)
+
+        code = click.prompt("Please enter validation code")
+        if not api.validate_verification_code(device, code):
+            print("Failed to verify verification code")
+            sys.exit(1)
+    api.drive.params["clientId"] = api.client_id
+    return api
+
+
+def is_dir(drive, folder):  # drive: api.drive, api.drive["folder"]...
+    if drive[folder].dir() is None:
+        return False
+    return True
+
+
+def build_folder_structure(api, icloud_folder, username, password):
+    # check if folder exists in iCloud Drive
+    if icloud_folder in api.drive.dir():
+        print(f"Folder {icloud_folder} already exists in iCloud Drive")
+        if is_dir(api.drive, icloud_folder):
+            print(f"Folder {icloud_folder} is a directory")
+        else:
+            print(
+                f"{icloud_folder} exists and it is not a directory. Please use another folder name"
+            )
+            sys.exit(1)
+    else:
+        api._drive.params["clientId"] = api.client_id
+        api.drive.mkdir(icloud_folder)
+        print(f"Created folder {icloud_folder} in iCloud Drive")
+        api = login(username, password)  # refresh api
+
+    api._drive.params["clientId"] = api.client_id
+    api.drive[icloud_folder].mkdir("images")
+    api.drive[icloud_folder].mkdir("videos")
+    api.drive[icloud_folder].mkdir("files")
+    api = login(username, password)  # refresh api
+
+    print("Created subfolders in iCloud Drive")
+    print("Ready to upload files")
+    return api
+
+
+def main(args):
+    username, password, icloud_folder = read_info(args.file)
+
+    folder_path = "./"
+    file_list = os.listdir(folder_path)
+    api = PyiCloudService(username, password)
+
+    api = build_folder_structure(api, icloud_folder, username, password)
+
+    image_extensions = ["png", "jpg", "jpeg", "gif", "bmp", "tiff", "ico"]
+    video_extensions = ["mp4", "avi", "mkv", "flv", "mov", "wmv"]
+
+    initial = True
+
+    while True:
+        # print("Checking files...")
+        current_file_list = os.listdir(folder_path)
+        new_files = set(current_file_list) - set(file_list)
+        if len(new_files) > 0:
+            print(f"new files: {new_files}")
+        for filename in new_files:
+            if not filename.endswith(".py") and not initial:
+                print(f"uploading {filename}")
+
+                if os.path.isdir(filename):
+                    # Compress directory to zip file and upload
+                    zip_filename = f"{filename}.zip"
+                    with zipfile.ZipFile(
+                        zip_filename, "w", zipfile.ZIP_DEFLATED
+                    ) as zipf:
+                        for root, dirs, files in os.walk(filename):
+                            for file in files:
+                                zipf.write(os.path.join(root, file))
+
+                    with open(zip_filename, "rb") as fp:
+                        try:
+                            api.drive[icloud_folder]["files"].upload(fp)
+                            print("upload zip file successfully")
+                        except Exception as e:
+                            print("Error:", e)
+
+                    # Remove the zip file after uploading
+                    os.remove(zip_filename)
+
+                else:
+                    with open(filename, "rb") as fp:
+                        try:
+                            extension = filename.split(".")[-1]
+                            if extension.lower() in image_extensions:
+                                target_folder = "images"
+                                print(
+                                    f"files identified as image, uploading to {target_folder}"
+                                )
+                            elif extension.lower() in video_extensions:
+                                target_folder = "videos"
+                                print(
+                                    f"files identified as video, uploading to {target_folder}"
+                                )
+                            else:
+                                target_folder = "files"
+                                print(
+                                    f"files identified as other, uploading to {target_folder}"
+                                )
+
+                            api.drive[icloud_folder][target_folder].upload(fp)
+                            print("upload successfully")
+                        except Exception as e:
+                            print("Error:", e)
+
+        file_list = current_file_list
+        initial = False
+        time.sleep(1 * 10)  # wait for 1 minutes before checking again
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Read username, password, and folder name from a file"
+    )
+    parser.add_argument(
+        "-f", "--file", default="info.txt", help="Input file (default: info.txt)"
+    )
+    args = parser.parse_args()
+    main(args)
